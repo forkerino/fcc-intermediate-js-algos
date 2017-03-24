76df566371e5bc3315f540c08cc63730b2c7d15f
forkerino <pieterstokkink@gmail.com>
2017-03-24 10:19:44 +0000

    roman numeral optimized

--- romannumeral.js
+++ romannumeral.js
@@ -4,14 +4,10 @@ Convert the given number into a roman numeral.
 All roman numerals answers should be provided in upper-case.*/
 
 function convertToRoman(num) {
-	const romans = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
-	const decimals = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  const rom = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+  const dec = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
 
-	return romans.reduce(function (pre, curr, i){ 
-        let rep = Math.floor(num/decimals[i]); 
-	    num %= decimals[i];
-	    return pre + curr.repeat(rep); 
-    },"");
+  return rom.reduce((p, c, i) => p + c.repeat((num - (num%=dec[i]))/dec[i]),""); 
 }
 
 // OLDER SOLUTION:
